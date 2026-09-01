6cad0cdc02ff2e78c4edeab5c1d545d6b1482d8e
Vinicius Miyata <viyudi@hotmail.com>
2026-09-01 15:56:14 -0300

    add a estrututura inicial do projeto

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Previsão de Demanda e Recomendação de Produção')
+"""
+Sistema Inteligente de Previsão de Demanda e
+Recomendação de Produção para uma Pastelaria.
+
+Trabalho de Conclusão de Curso - Ciência da Computação.
+
+"""
+
+
+def main() -> None:
+
+    print("=" * 60)
+    print("SISTEMA DE PREVISÃO DE DEMANDA")
+    print("E RECOMENDAÇÃO DE PRODUÇÃO")
+    print("=" * 60)
+
+
+if __name__ == "__main__":
+    main()
